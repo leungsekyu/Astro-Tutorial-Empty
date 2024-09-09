ff50f2b28170f32d95b4f3ea7284ac44f2334181
leungsekyu <leungsekyu@qq.com>
2024-09-09 19:45:55 +0800

    设置别名，将"@"映射到"/src"

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,3 +2,16 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  // 其他 Astro 配置项
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src', // 使用 '@' 指代 'src' 目录
+      },
+    },
+  },
+});
