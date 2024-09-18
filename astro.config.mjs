f103acf577ffe82a92046e91c1e7193e4e8752b2
leungsekyu <leungsekyu@qq.com>
2024-09-19 02:12:29 +0800

    添加了RSS订阅源

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,11 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
-
-import { defineConfig } from 'astro/config';
-
 export default defineConfig({
-  // 其他 Astro 配置项
+  site: 'https://astro-tutorial-empty.netlify.app/',
   vite: {
     resolve: {
       alias: {
-        '@': '/src', // 使用 '@' 指代 'src' 目录
+        '@': '/src',
       },
     },
   },
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,11 @@
+import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+
+export async function GET(context) {
+  return rss({
+    title: `leungsekyu's Blog`,
+    description: 'leungsekyu 的个人博客',
+    site: context.site,
+    items: await pagesGlobToRssItems(import.meta.glob('/src/**/*.md')),
+    customData: `<language>zh-cn</language>`,
+  });
+}
