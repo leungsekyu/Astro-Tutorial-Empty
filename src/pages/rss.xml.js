ad452ad8a784720aeb135819d1a89ffab382eaa5
leungsekyu <leungsekyu@qq.com>
2024-09-19 02:30:12 +0800

    将「`」引号换成「"」

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from '@astrojs/rss';
 
 export async function GET(context) {
   return rss({
-    title: `leungsekyu's Blog`,
+    title: "leungsekyu's Blog",
     description: 'leungsekyu 的个人博客',
     site: context.site,
     items: await pagesGlobToRssItems(import.meta.glob('/src/**/*.md')),
