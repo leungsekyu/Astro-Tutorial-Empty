39aa71772c8976a0d42ebedd783879177ee0feef
leungsekyu <leungsekyu@qq.com>
2024-09-20 03:34:45 +0800

    用内容集合重构了博客文章管理

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+  const posts = await getCollection('posts');
   return rss({
     title: "leungsekyu's Blog",
     description: 'leungsekyu 的个人博客',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('/src/**/*.md')),
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.slug}/`,
+    })),
     customData: `<language>zh-cn</language>`,
   });
 }
